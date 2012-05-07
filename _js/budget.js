f02f50b40ed0173c1807a490428fafa93482c409
Kyle Ouellette <kouellette@boston.com>
2012-05-07 12:36:52 -0400

    added starting content for js files and added script tags to index

---
+++ _js/budget.js
@@ -0,0 +1,10 @@
+(function(win, $, undefined){
+	var console = win.console || { log : function(){} };
+	win.Budget = {};
+
+	// add methods to Budget here
+	Budget.config = {
+		version : '1.0a'
+	};
+
+})(window, jQuery);
---
+++ _js/main.js
@@ -0,0 +1,6 @@
+(function(win, $, b, undefined){
+	var console = win.console || { log : function(){} };
+
+	// do stuff here
+	
+})(window, jQuery, Budget);
